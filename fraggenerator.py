ecb26de93c5a74059fa3726dcebd0ecd31d2564a
Rodrigo Rodrigues <rodrigo.rodrigues.rr.acc2@gmail.com>
2023-06-04 16:43:45 +0100

    Add effect generation.

--- fraggenerator.py
+++ fraggenerator.py
@@ -9,6 +9,8 @@ minLayers=1
 maxLayers=36
 def genAmmo():
     return [s.choice(['bullet','drone','swarm_drone','bee','overdrive','revitalist','navyist','whirlybird','trap','block','boomerang','sikerblock','laserline','ionlaser','apbullet','shellingbullet','missile','empmissile','nuke','empnuke','railgunslug','heavynuke','heavyempnuke','donutbullet','kbbullet','oapp_heatseeker','oappblackhole','ceptioner','ceptionist','missile','bomb','stabilizer','pillbox','trapboxes','taurusportal','minion','sunchip','oxyprojectile','laserblast']),fractions.Fraction(s.randint(1,60),6),fractions.Fraction(s.randint(1,60),6)]
+def genEffect():
+    return[s.choice('none','slide '+str(s.randint(5,73))+' of the Tank Effect Sheet'),'Level: '+str(s.randint(1,12)),'Time: '+str(s.randint(1,3600))+' s']
 if mode=="simple":
     l=[]
     for i in range(s.randint(minLayers,maxLayers+1)):
@@ -19,7 +21,7 @@ if mode=="simple":
 elif mode=="advanced":
     l=[]
     for i in range(s.randint(minLayers,maxLayers+1)):
-        l.append([s.randint(minFragNumber,maxFragNumber+1),genAmmo()])
+        l.append([s.randint(minFragNumber,maxFragNumber+1),genAmmo(),genEffect()])
     f.write(str(l))
     f.write('\n')
     f.close()

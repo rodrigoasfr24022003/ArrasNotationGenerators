ed40f6caface7d152bf8085c85c92a1318b271cc
Rodrigo Rodrigues <rodrigo.rodrigues.rr.acc2@gmail.com>
2023-06-04 16:58:48 +0100

    Added Pattern Generation

--- fraggenerator.py
+++ fraggenerator.py
@@ -10,7 +10,30 @@ maxLayers=36
 def genAmmo():
     return [s.choice(['bullet','drone','swarm_drone','bee','overdrive','revitalist','navyist','whirlybird','trap','block','boomerang','sikerblock','laserline','ionlaser','apbullet','shellingbullet','missile','empmissile','nuke','empnuke','railgunslug','heavynuke','heavyempnuke','donutbullet','kbbullet','oapp_heatseeker','oappblackhole','ceptioner','ceptionist','missile','bomb','stabilizer','pillbox','trapboxes','taurusportal','minion','sunchip','oxyprojectile','laserblast']),fractions.Fraction(s.randint(1,60),6),fractions.Fraction(s.randint(1,60),6)]
 def genEffect():
-    return[s.choice('none','slide '+str(s.randint(5,73))+' of the Tank Effect Sheet'),'Level: '+str(s.randint(1,12)),'Time: '+str(s.randint(1,3600))+' s']
+    return[s.choice(['none','slide '+str(s.randint(5,72))+' of the Tank Effect Sheet']),'Level: '+str(s.randint(1,12)),'Time: '+str(s.randint(1,3600))+' s']
+def genPattern():
+    def ConcentricCircleGen():
+        l=[]
+        for i in range(1,s.randint(2,13)):
+            if i<=4:
+                l.append(s.randint(1,36))
+            elif 5<=i<=7:
+                l.append(s.randint(1,72))
+            elif 8<=i<=10:
+                l.append(s.randint(1,108))
+            elif 11<=i<=13:
+                l.append(s.randint(1,144))
+        return l
+    def TwoDimGen():
+        l1=[]
+        for i in range(1,s.randint(2,37)):
+            l2=[]
+            for j in range(1,s.randint(2,37)): 
+                l2.append(s.randint(0,1))
+            l1.append(l2)
+        return l1
+    patterntype=s.choice(['none',s.choice(['rect'+str([s.randint(1,36),s.randint(1,36)]),'tri_'+s.choice(['l','f','r','b'])+str([s.randint(1,36),s.randint(1,36)]),'concentric_circles'+str(ConcentricCircleGen()),'line'+str([s.randint(1,36)]),'chevron'+str([2*s.randint(1,36)+1]),'cross'+str([4*s.randint(1,36)+1]),'advsquarepattern'+str(TwoDimGen())])])
+    return patterntype
 if mode=="simple":
     l=[]
     for i in range(s.randint(minLayers,maxLayers+1)):
@@ -21,7 +44,7 @@ if mode=="simple":
 elif mode=="advanced":
     l=[]
     for i in range(s.randint(minLayers,maxLayers+1)):
-        l.append([s.randint(minFragNumber,maxFragNumber+1),genAmmo(),genEffect()])
+        l.append([s.randint(minFragNumber,maxFragNumber+1),genAmmo(),genEffect(),genPattern()])
     f.write(str(l))
     f.write('\n')
     f.close()

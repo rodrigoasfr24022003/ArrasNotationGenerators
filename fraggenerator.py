ad7ceed45ef191c5ab6462b005d309d526ffa11b
Rodrigo Rodrigues <rodrigo.rodrigues.rr.acc2@gmail.com>
2023-06-04 16:38:20 +0100

    Add ammo generation

--- fraggenerator.py
+++ fraggenerator.py
@@ -1,11 +1,14 @@
 import random
+import fractions
 s=random.SystemRandom()
 f=open('fraggenresults.txt','w')
-mode="simple"
+mode="advanced"
 maxFragNumber=36
 minFragNumber=1
 minLayers=1
 maxLayers=36
+def genAmmo():
+    return [s.choice(['bullet','drone','swarm_drone','bee','overdrive','revitalist','navyist','whirlybird','trap','block','boomerang','sikerblock','laserline','ionlaser','apbullet','shellingbullet','missile','empmissile','nuke','empnuke','railgunslug','heavynuke','heavyempnuke','donutbullet','kbbullet','oapp_heatseeker','oappblackhole','ceptioner','ceptionist','missile','bomb','stabilizer','pillbox','trapboxes','taurusportal','minion','sunchip','oxyprojectile','laserblast']),fractions.Fraction(s.randint(1,60),6),fractions.Fraction(s.randint(1,60),6)]
 if mode=="simple":
     l=[]
     for i in range(s.randint(minLayers,maxLayers+1)):
@@ -13,3 +16,12 @@ if mode=="simple":
     f.write(str(l))
     f.write('\n')
     f.close()
+elif mode=="advanced":
+    l=[]
+    for i in range(s.randint(minLayers,maxLayers+1)):
+        l.append([s.randint(minFragNumber,maxFragNumber+1)])
+    f.write(str(l))
+    f.write('\n')
+    f.close()
+else:
+    raise ValueError("Invalid mode.")

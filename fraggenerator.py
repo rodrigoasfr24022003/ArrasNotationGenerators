cb570d1c3e5dd130d31acf303b15fe55edda4948
Rodrigo Rodrigues <rodrigo.rodrigues.rr.acc2@gmail.com>
2023-06-09 17:39:20 +0100

    fix some stuff

--- fraggenerator.py
+++ fraggenerator.py
@@ -11,7 +11,7 @@ for i in range(0,36000):
     def genAmmo():
         return [s.choice(['bullet','drone','swarm_drone','bee','overdrive','revitalist','navyist','whirlybird','trap','block','boomerang','sikerblock','laserline','ionlaser','apbullet','shellingbullet','missile','empmissile','nuke','empnuke','railgunslug','heavynuke','heavyempnuke','donutbullet','kbbullet','oapp_heatseeker','oappblackhole','ceptioner','ceptionist','missile','bomb','stabilizer','pillbox','trapboxes','taurusportal','minion','sunchip','oxyprojectile','laserblast','shrapnelnuke']),fractions.Fraction(s.randint(1,60),6),fractions.Fraction(s.randint(1,60),6)]
     def genEffect():
-        return[s.choices(['none','slide '+str(s.randint(5,74))+' of the Tank Effect Sheet','Pepperspray'],weights=[5,69,1],k=1)[0],'Level: '+str(s.randint(1,12)),'Time: '+str(s.randint(1,3600))+' s']
+        return[s.choices(['none','slide '+str(s.randint(5,76))+' of the Tank Effect Sheet','Pepperspray'],weights=[5,71,1],k=1)[0],'Level: '+str(s.randint(1,12)),'Time: '+str(s.randint(1,3600))+' s']
     def genPattern():
         def ConcentricCircleGen():
             l=[]
@@ -38,6 +38,7 @@ for i in range(0,36000):
             l1=[]
             for i in range (1,s.randint(2,37)):
                 l1.append(s.randint(1,36*i+1))
+            return l1
         patterntype=s.choice(['none',s.choice(['rect'+str([s.randint(1,36),s.randint(1,36)]),'tri_'+s.choice(['l','f','r','b'])+str([s.randint(1,36),s.randint(1,36)]),'concentric_circles'+str(ConcentricCircleGen()),'line'+str([s.randint(1,36)]),'chevron'+str([2*s.randint(1,36)+1]),'cross'+str([4*s.randint(1,36)+1]),'advsquarepattern'+str(TwoDimGen()),'advlinpattern'+str(LinPatternGen())])])
         return patterntype
     if mode=="simple":
